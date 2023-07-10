ca3f6898383c7b4c7d5c345f7326e9b4a0f516eb
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-10 09:42:16 +0200

    commit

--- main.js
+++ main.js
@@ -6,7 +6,25 @@ const manager = new Manager(todos);
 // console.log(todosList);
 
 function render() {
+    const todoContainer = document.getElementById('todo-container');
+
     for (const todo of manager.todoArray) {
-        
+        const div = document.createElement('div');
+
+        div.classList.add('todo-card');
+
+        const titleStrong = document.createElement('strong');
+        const titleNode = document.createTextNode(todo.title);
+
+        titleStrong.appendChild(titleNode);
+        div.appendChild(titleStrong);
+
+        const dateSpan = document.createElement('span');
+        const dateNode = document.createTextNode(todo.creationDate.toISOString());
+
+        dateSpan.appendChild(dateNode);
+        div.appendChild(dateSpan);
+        todoContainer.appendChild(div);
     }
 }
+render();
