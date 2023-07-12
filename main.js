503c3d1d17739532a13de2a2f4456e2261c2677e
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-12 09:36:00 +0200

    commit

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ function render() {
 
     todoContainer.innerHTML = '';
 
-    for (const todo of manager.todoArray) {
+    for (let i = 0; i < manager.todoArray.length; i++) {
+        const todo = manager.todoArray[i];
+
         const div = document.createElement('div');
         div.classList.add('todo-card');
 
@@ -32,45 +34,59 @@ function render() {
         div.appendChild(dateSpan);
 
         const completeBtn = document.createElement('button');
-        const completeNode = document.createTextNode('completato');
+        const completeNode = document.createTextNode(todo.isCompleted ? 'Completato' : ' Da Completare');
+        completeBtn.addEventListener('click', () => {
+            todo.isCompleted = !todo.isCompleted;
+            render();
+        });
 
-        completeBtn.addEventListener('click', () => {todo.isCompleted = true; render();});
         completeBtn.appendChild(completeNode);
-        div.appendChild(completeNode);
+        div.appendChild(completeBtn);
+
+        const deleteBtn = document.createElement('button');
+        const deleteNode = document.createTextNode('Elimina');
+        deleteBtn.addEventListener('click', () => {
+            manager.deleteTodo(i);
+            render();
+        });
+        
+        deleteBtn.appendChild(deleteNode);
+        div.appendChild(deleteBtn);
+
         todoContainer.appendChild(div);
     }
 }
 render();
 
-function render2() {
-    const todoContainer = document.getElementById('todo-container'); 
+// function render2() {
+//     const todoContainer = document.getElementById('todo-container'); 
 
-    todoContainer.innerHTML = '';
+//     todoContainer.innerHTML = '';
 
-    for (let i = 0; i < manager.todoArray.length; i++) {
-        const todo = manager.todoArray[i];
-        let additionalClass = '';
+//     for (let i = 0; i < manager.todoArray.length; i++) {
+//         const todo = manager.todoArray[i];
+//         let additionalClass = '';
 
-        if (todo.isCompleted) {
-            additionalClass = 'todo-completed';
-        }
+//         if (todo.isCompleted) {
+//             additionalClass = 'todo-completed';
+//         }
 
-        const template = `<div class="todo-card ${additionalClass}"><strong>${todo.title}</strong><span>${todo.creationDate.toISOString()}</span><button id="complete-btn"${i}>completato</button></div>`;
+//         const template = `<div class="todo-card ${additionalClass}"><strong>${todo.title}</strong><span>${todo.creationDate.toISOString()}</span><button id="complete-btn"${i}>completato</button></div>`;
     
 
 //     const template = `<div class="todo-card ${todo.isCompleted ? 'todo-completed' : ''}"><strong>${todo.title}</strong><span>${todo.creationDate.toISOString()}</span></div>`;
 
-    todoContainer.innerHTML += template;
-    }
+//     todoContainer.innerHTML += template;
+//     }
 
-    for (let i = 0; i < manager.todoArray.length; i++) {
-        const todo = manager.todoArray[i];
-        const completeBtn = document.getElementById('complete-btn' + i);
+//     for (let i = 0; i < manager.todoArray.length; i++) {
+//         const todo = manager.todoArray[i];
+//         const completeBtn = document.getElementById('complete-btn' + i);
 
-        completeBtn.addEventListener('click', () => {todo.isCompleted = !todo.isCompleted; render2();});
-    }
-}
-render2();
+//         completeBtn.addEventListener('click', () => {todo.isCompleted = !todo.isCompleted; render2();});
+//     }
+// }
+// render2();
 
 function orderByTitle() {
     manager.orderTodosByTitle();
@@ -82,5 +98,12 @@ function orderByDate() {
     render();
 }
 
-// 1) aggiungere il tasto cancella su ogni todo
-// 2) aggiungere la possibiltà di aggiungere
+function addTodo() {
+    const title = document.getElementById('add-todo-input');
+    const newTodoTitle = title.value;
+    
+    if (newTodoTitle.trim() !== '') {
+        manager.addTodoWithTitle(newTodoTitle);
+    }
+    render();
+}
